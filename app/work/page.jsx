3d6498c51f26b7a89f1d92983eb557cf1ec2238d
hhsaputraa <hhsptr12@gmail.com>
2024-08-29 16:32:19 +0700

    slider in work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -10,13 +10,15 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 
 import Link from "next/link";
 import Image from "next/image";
+import { handleClientScriptLoad } from "next/script";
+import WorksliderButton from "@/components/WorksliderButton";
 
 const projects = [
   {
     num: "01",
     category: "frontend",
     title: "project 1",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe vel, soluta magnam at vero unde doloremque numquam ipsum quaerat recusandae, accusamus quod illo",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     stack: [{ name: "Html 5 " }, { name: "Javascript" }, { name: "Html 5 " }, { name: "Html 5 " }],
     image: "/assets/work/thumb1.png",
     live: "",
@@ -26,7 +28,7 @@ const projects = [
     num: "02",
     category: "frontend",
     title: "project 1",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe vel, soluta magnam at vero unde doloremque numquam ipsum quaerat recusandae, accusamus quod illo",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     stack: [{ name: "Html 5 " }, { name: "Javascript" }, { name: "Html 5 " }, { name: "Html 5 " }],
     image: "/assets/work/thumb2.png",
     live: "",
@@ -36,7 +38,7 @@ const projects = [
     num: "03",
     category: "frontend",
     title: "project 3",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe vel, soluta magnam at vero unde doloremque numquam ipsum quaerat recusandae, accusamus quod illo",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     stack: [{ name: "Html 5 " }, { name: "Javascript" }, { name: "Html 5 " }, { name: "Html 5 " }],
     image: "/assets/work/thumb1.png",
     live: "",
@@ -45,12 +47,19 @@ const projects = [
 ];
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    //get current slide index
+    const currentIndex = swiper.activeIndex;
+    //update project state based on current slide index
+    setProject(projects[currentIndex]);
+  };
   return (
-    <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0 ">
+    <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0 ">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30-px]">
-          <div className="w-full xl:-w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div className="flex flex-col gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
               {/* outline num */}
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
             </div>
@@ -101,7 +110,26 @@ const Work = () => {
               </Link>
             </div>
           </div>
-          <div className="w-full xl:-w-[50%]">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      {/* overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      {/* image */}
+                      <div className="relative w-full h-full">
+                        <Image src={project.image} fill className="object-cover" alt="" />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              {/* slider button */}
+              <WorksliderButton containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" />
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
